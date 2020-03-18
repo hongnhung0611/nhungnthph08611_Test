15bdae0e92ce827eeb19cf71993586a2bbbc2776
hongnhung0611 <hongnhungvananh@gmail.com>
2020-03-18 15:33:56 +0700

    4 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,56 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+
+// import { AppComponent } from './app.component';
+// import { HelloComponent } from './hello.component';
+// import { ProductListComponent } from './product-list/product-list.component';
+// import { SlideComponent } from './slide/slide.component';
+// import { ProductComponent } from './product/product.component';
+// import { CategoryComponent } from './category/category.component';
+
+// import {FormsModule} from  '@angular/forms';
+
+// @NgModule({
+//   imports:      [ BrowserModule, FormsModule ],
+//   declarations: [ AppComponent, HelloComponent, ProductListComponent, SlideComponent, ProductComponent, CategoryComponent ],
+//   bootstrap:    [ AppComponent ]
+// })
+// export class AppModule { }
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { ProductListComponent } from './product-list/product-list.component';
+// import { CpntestComponent } from './cpntest/cpntest.component';
+import { WordComponent } from './word.component';
 import { SlideComponent } from './slide/slide.component';
 import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { CategoryComponent } from './category/category.component';
-
+import { TestComponent } from './test/test.component';
+import { CtvComponent } from './ctv/ctv.component';
+import {FormsModule} from '@angular/forms';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductListComponent, SlideComponent, ProductComponent, CategoryComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    // CpntestComponent,
+    // WordComponent,
+    SlideComponent,
+    ProductComponent,
+    ProductListComponent,
+    CategoryComponent,
+    // TestComponent,
+    // CtvComponent
+  ],
+  imports: [
+    FormsModule,
+    // AppRoutingModule,
+    BrowserModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../Product';
 
 @Component({
   selector: 'app-product-list',
@@ -9,7 +10,29 @@ export class ProductListComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
+  // name : string =' item1';
+  // price : number = 20;
+  // desc : string = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+  // status : boolean = true
+  product: Product={
+    id :1,
+    name: "213",
+    price : 20000,
+    desc :'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! aaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    status : false,
+    img : 'https://dummyimage.com/275x175/000/fff?fbclid=IwAR3bO5qngYKlzkgox65Ui-HdgJTCFMZkZaemLSMoUHKsg8MeDcaGAPBzWd4'
+    
+
+  }
+  changeStatus(){
+    this.product.status = !this.product.status;
+  }
+  changeName(e){
+    this.product.name = e.target.value;
+  }
+ 
+
 
 }
