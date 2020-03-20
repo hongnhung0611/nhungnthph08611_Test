c1bf3cd712d0728d9c90aeadbbd53d8518f9afa3
hongnhung0611 <hongnhungvananh@gmail.com>
2020-03-20 22:29:12 +0700

    5 files modified

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import{Product} from '../Product';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+@Input('data') product: Product;
   constructor() { }
 
   ngOnInit() {
