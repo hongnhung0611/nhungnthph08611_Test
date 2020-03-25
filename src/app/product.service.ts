f08259daf6eed9167e35b9a6c1ccfce551c071e2
hongnhung0611 <hongnhungvananh@gmail.com>
2020-03-25 19:06:21 +0700

    2 files modified

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,40 +2,37 @@ import { Injectable } from "@angular/core";
 import { data } from "./MockData";
 import { Product } from "./Product";
 import { HttpClient } from "@angular/common/http";
-import { Observable} from 'rxjs';
+import { Observable } from "rxjs";
 @Injectable()
 export class ProductService {
   // products = data;
-  api = 'https://5e79b4b817314d00161333da.mockapi.io/product';
+  api = "https://5e79b4b817314d00161333da.mockapi.io/product";
   // api : 'https://5e7a380817314d0016133ec3.mockapi.io/products';
-  constructor(
-    private http : HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
-  
-
-  getProducts() : Observable<Product[]> { //danh sách sp
+  getProducts(): Observable<Product[]> {
+    //danh sách sp
     // return this.products;
     return this.http.get<Product[]>(this.api);
   }
-  getProduct(id): Observable<Product> { 
+  getProduct(id): Observable<Product> {
     // return this.products.find(product => product.id == id);
     return this.http.get<Product>(`${this.api}/${id}`);
   }
-  updateProduct(product): Observable<Product>{
-return this.http.put<Product>(`${this.api}/${product.id}` , product);
+  updateProduct(product): Observable<Product> {
+    return this.http.put<Product>(`${this.api}/${product.id}`, product);
   }
-
-
+  addProduct(product): Observable<Product> {
+    return this.http.post<Product>(`${this.api}`, product);
+    // const newProduct = { id: 5, ...product };
+    // this.products.push(newProduct);
+    // console.log(this.products);
+  }
 
   removeProduct(id) {
     return this.products.filter(product => product.id != id);
   }
-  addProduct(product) {
-    const newProduct = { id: 5, ...product };
-    this.products.push(newProduct);
-    console.log(this.products);
-  }
+
   editProduct() {
     // sua san pham
   }
