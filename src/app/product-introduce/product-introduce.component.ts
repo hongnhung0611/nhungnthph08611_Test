315c68fe52baa3f405c19e6b497361066b1d3e73
hongnhung0611 <hongnhungvananh@gmail.com>
2020-03-26 14:50:23 +0700

    1 file modified, 2 added

---
+++ src/app/product-introduce/product-introduce.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-introduce',
+  templateUrl: './product-introduce.component.html',
+  styleUrls: ['./product-introduce.component.css']
+})
+export class ProductIntroduceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
